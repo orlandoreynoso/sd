d8dffd9339f8c9bf8d371e01c00e351958b0a325
orlandoreynoso <orlandoreynoso01@gmail.com>
2018-04-26 16:46:05 -0600

    delete

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 	<title><?php bloginfo('name'); ?></title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.ico" type="image/x-icon" />
-	<?php wp_head(); ?>
 	<?php get_template_part( 'template/codigo', 'seguimiento'); ?>
+	<?php wp_head(); ?>
 
 </head>
 <body>
